e29a5fd5f6a65bee22daf9e34fbc6d823529beda
Faustine123 <yx.fan@mail.utoronto.ca>
2023-03-02 11:20:30 -0500

    simulate

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,27 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
 # [...ADD CODE HERE...]
 
+set.seed(200)
 
+# prepare eight cups of tea: four with milk added first and the other four with milk added last
+cups <- rep(c("milk_first","tea_first"), each = 4)
 
+#randomize the order of all eight cups
+random_cups <- sample(cups)
+
+#create a dataset of Ian's tea groupings in four separate experiments
+experimnet1 <- sample(random_cups, size = 8)
+experimnet2 <- sample(random_cups, size = 8)
+experimnet3 <- sample(random_cups, size = 8)
+experimnet4 <- sample(random_cups, size = 8)
+tea_data <- data.frame(experiment = rep(1:4, each = 8),
+                       cup_number = 1:8,
+                       cup_type = random_cups,
+                       tester_grouping) = c(experiment1, experiment2, experiment3, experiment4)
+  mutate(result = ifelse(cup_type == tester_grouping, "Right","Wrong"))
+
+tea_data
